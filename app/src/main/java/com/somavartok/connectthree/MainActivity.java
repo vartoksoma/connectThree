dd638f7975c32b127249a7508b7eb220d9211708
Vartok Soma <vartok.soma@gmail.com>
2019-02-20 11:09:43 +0100

    added counter and background grid images, setup the board, did first animation forcounter

--- app/src/main/java/com/somavartok/connectthree/MainActivity.java
+++ app/src/main/java/com/somavartok/connectthree/MainActivity.java
@@ -2,8 +2,22 @@ package com.somavartok.connectthree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1000f).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
